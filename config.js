2ef711a1702698925d0be0bc72b327ac63e51460
Otis0309 <130805472+Otis0309@users.noreply.github.com>
2025-04-01 17:04:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const MENU_API_URL = "https://script.google.com/macros/s/AKfycbyrhN-d9Rp8pXTmZr_Yo4RGRQgC4VDwg3XF88oRQob9BdEsRh9tff_zcuTtyFhUqyz4oA/exec?action=getMenu";
 
 // 提交訂單用的 API（例如 POST）
-const SUBMIT_API_URL = "https://script.google.com/macros/s/AKfycbyAwIuZOeFREAtspq00R5IktbyQfE2qN4c8oUAihIBh/exec";
+const SUBMIT_API_URL = "https://script.google.com/macros/s/AKfycbyrhN-d9Rp8pXTmZr_Yo4RGRQgC4VDwg3XF88oRQob9BdEsRh9tff_zcuTtyFhUqyz4oA/exec";
 
 // 表單設定
 const MAX_QUANTITY = 10;
