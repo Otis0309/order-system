f18e2a7641f162a8a9a21f7a692022f4810e7409
Otis0309 <130805472+Otis0309@users.noreply.github.com>
2025-04-01 18:02:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // GAS Web App 的網址
-const MENU_API_URL = "https://script.google.com/macros/s/AKfycbyrhN-d9Rp8pXTmZr_Yo4RGRQgC4VDwg3XF88oRQob9BdEsRh9tff_zcuTtyFhUqyz4oA/exec?action=getMenu";
+const MENU_API_URL = "https://script.google.com/macros/s/AKfycbyZ-fULDhgO2ZZgDQ3NO01PGSntp2GecelXuP3P_RJKXxjXpdvc9uGbhzuhcyPK_w72cA/exec?action=getMenu";
 
 // 提交訂單用的 API（例如 POST）
-const SUBMIT_API_URL = "https://script.google.com/macros/s/AKfycbyrhN-d9Rp8pXTmZr_Yo4RGRQgC4VDwg3XF88oRQob9BdEsRh9tff_zcuTtyFhUqyz4oA/exec";
+const SUBMIT_API_URL = "https://script.google.com/macros/s/AKfycbyZ-fULDhgO2ZZgDQ3NO01PGSntp2GecelXuP3P_RJKXxjXpdvc9uGbhzuhcyPK_w72cA/exec";
 
 // 表單設定
 const MAX_QUANTITY = 10;
