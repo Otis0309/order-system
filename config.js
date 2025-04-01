f6458420c81646bbb53fe91bcd042f4e1ffa7a85
Otis0309 <130805472+Otis0309@users.noreply.github.com>
2025-04-01 21:32:08 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// GAS Web App 的網址
-const MENU_API_URL = "https://script.google.com/macros/s/AKfycbyZ-fULDhgO2ZZgDQ3NO01PGSntp2GecelXuP3P_RJKXxjXpdvc9uGbhzuhcyPK_w72cA/exec?action=getMenu";
-
-// 提交訂單用的 API（例如 POST）
-const SUBMIT_API_URL = "https://script.google.com/macros/s/AKfycbyZ-fULDhgO2ZZgDQ3NO01PGSntp2GecelXuP3P_RJKXxjXpdvc9uGbhzuhcyPK_w72cA/exec";
-
-// 表單設定
-const MAX_QUANTITY = 10;
